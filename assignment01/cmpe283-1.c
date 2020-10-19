d0a1c9d1478e28d74d9e7a855b1b3c20e514f6ac
ruiyang <reyyang00@gmail.com>
2020-10-18 17:04:49 -0700

    finish all detect functions on vm control fields for pinbased, procbased(primary, secondary), exit, entry

--- assignment01/cmpe283-1.c
+++ assignment01/cmpe283-1.c
@@ -11,9 +11,12 @@
  * Model specific registers (MSRs) by the module.
  * See SDM volume 4, section 2.1
  */
-#define IA32_VMX_PINBASED_CTLS	0x481
+#define IA32_VMX_PINBASED_CTLS 0x481
+#define IA32_VMX_PROCBASED_CTLS 0x482
+#define IA32_VMX_PROCBASED_CTLS2 0x48B
+#define IA32_VMX_EXIT_CTLS 0x483
+#define IA32_VMX_ENTRY_CTLS 0x484
 
-#define IA32_VMX_PROCBASED_CTLS	0x482
 /*
  * struct caapability_info
  *
@@ -66,7 +69,72 @@ struct capability_info procbased[21] =
 };
 
 
+struct capability_info procbased2[27] =
+{
+	{ 0, "VIRTUALIZE_APIC_ACCESSES" },
+	{ 1, "ENABLE_EPT" },
+	{ 2, "DESCRIPTOR_TABLE_EXITING" },
+	{ 3, "ENABLE_RDTSCP" },
+	{ 4, "VIRTUALIZE_X2APIC_MODE" },
+	{ 5, "ENABLE_VPID" },
+	{ 6, "WBINVD_EXITING" },
+	{ 7, "UNRESTRICTED_GUEST" },
+	{ 8, "APIC_REGISTER_VIRTUALIZATION" },
+	{ 9, "VIRTUAL_INTERRUPT_DELIVERY" },
+	{ 10, "PAUSE_LOOP_EXITING" },
+	{ 11, "RDRAND_EXITING" },
+	{ 12, "ENABLE_INVPCID" },
+	{ 13, "ENABLE_VM_FUNCTIONS" },
+	{ 14, "VMCS_SHADOWING" },
+	{ 15, "ENABLE_ENCLS_EXITING" },
+	{ 16, "RDSEED_EXITING" },
+	{ 17, "ENABLE_PML" },
+	{ 18, "EPT_VIOLATION_#VE" },
+	{ 19, "CONCEAL_VMX_FROM_PT" },
+	{ 20, "ENABLE_XSAVES/XRSTORS" },
+	{ 22, "MODE_BASED_EXECUTE_CONTROL_FOR_EPT"},
+	{ 23, "SUB_PAGE_WRITE_PERMISSIONS_FOR_EPT"},
+	{ 24, "INTEL_PT_USES_GUEST_PHYSICAL_ADDRESSES"},
+	{ 25, "USE_TSC_SCALING"},
+	{ 26, "ENABLE_USER_WAIT_AND_PAUSE"},
+	{ 28, "ENABLE_ENCLV_EXITING"}
+};
 
+
+struct capability_info exit[14] =
+{
+	{ 2, "SAVE_DEBUG_CONTROLS"},
+	{ 9, "HOST_ADDRESS_SAPCE_SIZE"},
+	{ 12, "LOAD_IA32_PERF_GLOBAL_CTRL"},
+	{ 15, "ACKNOWLEDGE_INTERRUPT_ON_EXIT"},
+	{ 18, "SAVE_IA32_PAT"},
+	{ 19, "LOAD_IA32_PAT"},
+	{ 20, "SAVE_IA32_EFER"},
+	{ 21, "LOAD_IA32_EFER"},
+	{ 22, "SAVE_VMX_PREEMPTION_TIMER_VALUE"},
+	{ 23, "CLEAR_IA32_BNDCFGS"},
+	{ 24, "CONCEAL_VMX_FROM_PT"},
+	{ 25, "CLEAR_IA32_RTIT_CTL"},
+	{ 28, "LOAD_CET_STATE"},
+	{ 29, "LOAD_PKRS"}
+};
+
+
+struct capability_info entry[12] =
+{
+	{ 2, "LOAD_DEBUG_CONTROLS"},
+	{ 9, "IA_32E_MODE_GUEST"},
+	{ 10, "ENTRY_TO_SMM"},
+	{ 11, "DEACTIVATE_DUAL_MONITOR_TREATMENT"},
+	{ 13, "LOAD_IA32_PERF_GLOBA_L_CTRL"},
+	{ 14, "LOAD_IA32_PAT"},
+	{ 15, "LOAD_IA32_EFER"},
+	{ 16, "LOAD_IA32_BNDCFGS"},
+	{ 17, "CONCEAL_VMX_FROM_PT"},
+	{ 18, "LOAD_IA32_RTIT_CTL"},
+	{ 20, "LOAD_CET_STATE"},
+	{ 22, "LOAD_PKRS"}
+};
 /*
  * report_capability
  *
@@ -114,13 +182,35 @@ detect_vmx_features(void)
 	pr_info("Pinbased Controls MSR: 0x%llx\n",
 		(uint64_t)(lo | (uint64_t)hi << 32));
 	report_capability(pinbased, 5, lo, hi);
-
 	
 	/* Procbased controls */
 	rdmsr(IA32_VMX_PROCBASED_CTLS, lo, hi);
 	pr_info("Procbased Controls MSR: 0x%llx\n",
 		(uint64_t)(lo | (uint64_t)hi << 32));
 	report_capability(procbased, 21, lo, hi);
+
+	
+	/* Procbased secondary controls */
+	rdmsr(IA32_VMX_PROCBASED_CTLS2, lo, hi);
+	pr_info("Procbased Secondary Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(procbased2, 27, lo, hi);
+
+	
+	/* Exit controls */
+	rdmsr(IA32_VMX_EXIT_CTLS, lo, hi);
+	pr_info("Exit Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(exit, 14, lo, hi);
+
+	
+	/* Entry controls */
+	rdmsr(IA32_VMX_ENTRY_CTLS, lo, hi);
+	pr_info("Entry Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(entry, 12, lo, hi);
+
+
 }
 
 /*
